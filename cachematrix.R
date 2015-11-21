9722503d02e0cb387704157d2378bc554b622d21
John Jacob <jjacob_ru@yahoo.co.uk>
2015-11-21 22:42:37 +0000

    Function body completed
    function definitions added to assignment template

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cacheMatrix is a matrix object for efficiently retrieving the inverse of a matrix. the inverse is computed
+## once (the first time the inverse is requested) and cached for future use
 
-## Write a short comment describing this function
+## Function to create a matrix object with cached inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    xI <- NULL
+    set <- function(y) {
+        x <<- y
+        xI <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inv) xI <<- inv
+    getInverse <- function() xI
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Cached version of matrix inversion function - calculates and caches the inverse on first invocation,
+## returning the cached value on subsequent calls
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    xI <- x$getInverse()
+    if (!is.null(xI)) {
+        message("Using cached value of inverse matrix")
+        return(xI)
+    }
+    xData <- x$get()
+    xI<-solve(xData)
+    x$setInverse(xI)
+    xI
 }
+
